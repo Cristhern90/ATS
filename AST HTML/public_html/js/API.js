5e3102a208590e6e3960fbc36dc41cc7ff7e7bbc
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2025-03-13 22:09:59 +0100

    API conect all functions

--- AST HTML/public_html/js/API.js
+++ AST HTML/public_html/js/API.js
@@ -14,5 +14,15 @@ function API_query(url, method, timeout) {
 
 function get_game_achievement(game, lang) {
     let url = 'https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=' + key + '&appid=' + game + '&l=' + lang;
-    API_query(url,"GET",0);
+    API_query(url, "GET", 0);
+}
+
+function get_game_achievement_global(game) {
+    let url = 'https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=' + game + '&format=XML';
+    API_query(url, "GET", 0);
+}
+
+function get_game_my_achievements(game, steamid, lang) {
+    let url = 'https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=' + game + '&key=' + key + '&steamid=' + steamid + '&l=' + lang;
+    API_query(url, "GET", 0);
 }
